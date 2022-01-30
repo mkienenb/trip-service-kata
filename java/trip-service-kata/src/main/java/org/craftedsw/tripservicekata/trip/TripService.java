74db34a84ab15ed27e2d1c905d781d7d6f6f36d7
Mike Kienenberger <mkienenb@gmail.com>
2022-01-30 17:01:30 -0500

    Javadocs

--- java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -9,6 +9,11 @@ import java.util.List;
 
 public class TripService {
 
+    /**
+     * @param user which is a friend of logged-in user
+     * @return List of trips if logged-in user is a friend of user, otherwise empty list.
+     * @throws UserNotLoggedInException if no user logged in
+     */
     public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
         User loggedUser = getLoggedUser();
         if (loggedUser == null) {
